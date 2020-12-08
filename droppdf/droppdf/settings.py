2d4c19930234216cde93f585e814f5ab59441f79
paul <paul.genho@gmail.com>
2020-12-08 12:15:37 -0700

    add .env file for settings

--- droppdf/droppdf/settings.py
+++ droppdf/droppdf/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env('.env')
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
