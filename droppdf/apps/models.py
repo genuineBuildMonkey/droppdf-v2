4c0dab1a738138874851e72b5473a4c1459234bb
paul <paul.genho@gmail.com>
2021-01-21 19:32:14 -0700

    upload models and settings

--- droppdf/apps/models.py
+++ droppdf/apps/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class FileUload(models.Model):
+    '''reference to cloud upload'''
+    name = models.CharField(max_length=35)
+
+    url = models.CharField(max_length=50)
+
+    extension = models.CharField(max_length=8)
+
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    updated = models.DateTimeField(auto_now=True)
