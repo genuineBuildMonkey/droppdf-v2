1a27db28b212ef603e6d0a9317c46e3bc1317002
paul <paul.genho@gmail.com>
2021-02-21 11:40:35 -0700

    restore youtube video annotator. fix problems for new django

--- droppdf/apps/apps/urls.py
+++ droppdf/apps/apps/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from django.urls import include
 
-urlpatterns = [path('', include('apps.apps.annotator.urls'))]
+urlpatterns = [path('', include('apps.apps.annotator.urls')),
+        path('', include('apps.apps.video.urls'))
+        ]
